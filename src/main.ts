18492819888d8123c47b7fce9ac5d9c987fc6db9
codeslw <sanvarkhujaev@gmail.com>
2025-03-25 05:18:58 +0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
-
+import { Request, Response } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
@@ -30,7 +30,21 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+  //i want to send the swagger-spec.json file to the frontend when the swagger is opened
+  
+  app.getHttpAdapter().get('/swagger-spec.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(document);
+  });
+
+  // Ensure the Swagger UI fetches the spec when opened
+  app.getHttpAdapter().use('/api', (req, res, next) => {
+    if (req.path === '/swagger-ui.html') {
+      res.redirect('/swagger-spec.json');
+    } else {
+      next();
+    }
+  });
   
   // Enable CORS
   app.enableCors({
