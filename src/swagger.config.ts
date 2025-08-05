ac3f4b9c59f07d22108846233371dddbf50928b2
codeslw <sanvarkhujaev@gmail.com>
2025-08-05 07:08:17 +0500

    fixed

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('NestJS Prisma Auth API')
   .setDescription('API with authentication, roles, and PostgreSQL')
-  .setVersion('1.0')
+  .setVersion('1.0.1')
   .addBearerAuth(
     { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
     'JWT-auth', // Name of the security scheme
